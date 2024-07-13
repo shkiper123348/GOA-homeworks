f6205561298c62f7abacb325bc9111da10838eac
alex chitanava <alextchitanava@gmail.com>
2024-07-13 23:18:07 +0400

    Update homework5.py

--- level 6/homework5.py
+++ level 6/homework5.py
@@ -1,7 +1,7 @@
-number1 = 18
-number2 = 3
+number1 = input()
+number2= input()
 
-print (number1 + number2)
-print (number1 - number2)
-print (number1 * number2)
-print (number1 / number2)
+print(int(number1) - int(number2))
+print(int(number1) + int(number2))
+print(int(number1) * int(number2))
+print(int(number1) / int(number2))
