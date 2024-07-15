e98ca22d8cd9c4a8dab0b7895c5b64668a1392b3
shkiper123348 <alextchitanava@gmail.com>
2024-07-15 14:38:16 +0400

    commit

---
+++ level 6/homework/homework5.py
@@ -0,0 +1,7 @@
+number1 = input()
+number2 = input()
+
+print(int(number1) - int(number2))
+print(int(number1) + int(number2))
+print(int(number1) * int(number2))
+print(int(number1) / int(number2))
--- level 6/homework5.py
+++
@@ -1,7 +0,0 @@
-number1 = 18
-number2 = 3
-
-print (number1 + number2)
-print (number1 - number2)
-print (number1 * number2)
-print (number1 / number2)
